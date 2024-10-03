1c5fa4ce47a16992d44f24b4052591ff32a86825
Hunar Batra <i@hunarbatra.com>
2024-10-03 16:02:33 +0100

    update rush.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 # install packages and load in editable mode
 pip install -e .
-pip install flash-attn --no-build-isolation --no-cache-dir
+pip uninstall torch torchvision flash-attn -y
+pip install flash-attn --no-cache-dir --no-build-isolation
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 
 # download data
-python download_data.py pretrain_data
-python download_data.py finetune_data # takes 1-2 hours
+python download_data.py pretrain_data --json_only=True
+python download_data.py finetune_data --json_only=True
 
 # get base model
 bash scripts/pixllava/get_base_model.sh
