8c1bd56d1c202fbc03b1c267a98508d46a000b10
Hunar Batra <i@hunarbatra.com>
2024-10-28 16:07:26 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,7 @@
 
 # install packages and load in editable mode
 pip install -e .
-pip uninstall torch torchvision flash-attn -y
 pip install flash-attn --no-cache-dir --no-build-isolation
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 
 # download data
 python download_data.py pretrain_data --json_only=True
