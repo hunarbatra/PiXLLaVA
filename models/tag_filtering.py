7a9919c8d6b45eb47a3b3849c85da900f101ddb6
Hunar Batra <i@hunarbatra.com>
2024-11-27 09:31:44 +0000

    add async calls tag filtering

--- models/tag_filtering.py
+++ models/tag_filtering.py
@@ -1,22 +1,61 @@
 import os
-import ell
+import backoff
+import threading
+import concurrent.futures
+import openai
+import anthropic
 
-from openai import OpenAI
+from openai import OpenAI, OpenAIError, RateLimitError
+
 from models.prompt import TAG_FILTER_SYSTEM_PROMPT
 
 
 openai_key = os.getenv('OPENAI_API_KEY')
+oai_client = OpenAI(api_key=openai_key)
 
-def filter_tags(tags: str, prompt: str):
-    ell.init(store='./logdir', autocommit=True)
+anthropic_key = os.getenv('ANTHROPIC_API_KEY')
+ant_client = anthropic.Anthropic(api_key=anthropic_key)
 
-    @ell.simple(model="gpt-4o-mini", temperature=0, client=OpenAI(api_key=openai_key))
-    def call_llm(tags: str, prompt: str):
-        return [
-            ell.system(TAG_FILTER_SYSTEM_PROMPT),
-            ell.user(f'''Prompt: {prompt}
-                     Tags: {tags}
-                    ''')
+@backoff.on_exception(backoff.expo, (OpenAIError, RateLimitError), max_tries=10)
+def filter_tags_openai(tags: str, prompt: str):
+    completion = oai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": TAG_FILTER_SYSTEM_PROMPT},
+            {"role": "user", "content": f"Prompt: {prompt}\nTags: {tags}\nRelevant Tags:"}
+        ],
+        temperature=0.7,
+    )
+    return completion.choices[0].message.content
+
+@backoff.on_exception(backoff.expo, (RateLimitError), max_tries=10)
+def filter_tags_anthropic(tags: str, prompt: str):
+    message = ant_client.messages.create(
+        model="claude-3-5-haiku-20241022",
+        temperature=0.0,
+        max_tokens=50,
+        messages=[
+            {"role": "system", "content": TAG_FILTER_SYSTEM_PROMPT},
+            {"role": "user", "content": f"Prompt: {prompt}\nTags: {tags}\nRelevant Tags:"}
         ]
+    )
+    return message.content
 
-    return call_llm(tags, prompt)
+def process_tags_batch(prompts: list[dict], model='openai'):
+    def worker(prompt_data):
+        try:
+            original_tags = prompt_data['tags']
+            if model == 'openai':
+                filtered_tags = filter_tags_openai(prompt_data['tags'], prompt_data['prompt'])
+            elif model == 'anthropic':
+                filtered_tags = filter_tags_anthropic(prompt_data['tags'], prompt_data['prompt'])
+            # print(f'all_tags: {original_tags} | filtered_tags: {filtered_tags}')
+            return filtered_tags
+        except Exception as e:
+            print(f"Error processing prompt: {prompt_data['prompt']}: {e}")
+            return e
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(worker, prompts))
+            
+    return results
