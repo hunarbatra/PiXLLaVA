6b1d7929b446475fd52231d3f5e7d2141c0e0a3d
Hunar Batra <i@hunarbatra.com>
2024-12-03 21:12:07 +0000

    add llama model for trainer

--- pixl/model/__init__.py
+++ pixl/model/__init__.py
@@ -2,5 +2,6 @@ from .language_model.pixl_phi import PIXLPhiForCausalLM
 from .language_model.pixl_gemma import PIXLGemmaForCausalLM
 from .language_model.pixl_phi3 import PIXLPhi3ForCausalLM
 from .language_model.pixl_phi1_5 import PIXLPhi15ForCausalLM
+from .language_model.pixl_llama import PIXLlamaForCausalLM
 from .language_model.configuration_pixl import PIXLVisionConfig, ProjectorConfig, PIXLPhiConfig, PIXLGemmaConfig, \
-    PIXLPhi3Config, PIXLPhi15Config
+    PIXLPhi3Config, PIXLPhi15Config, PIXLlamaConfig
