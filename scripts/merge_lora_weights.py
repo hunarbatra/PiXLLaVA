817eb2d5b60d1e8c701538a8e0a2fe01734b26f5
Hunar Batra <i@hunarbatra.com>
2024-12-09 22:24:35 +0000

    update merge lora weights script

--- scripts/merge_lora_weights.py
+++ scripts/merge_lora_weights.py
@@ -1,6 +1,17 @@
+import os
 import argparse
+
+from huggingface_hub import HfApi
+from dotenv import load_dotenv
+
 from pixl.model.builder import load_pretrained_model
 from pixl.mm_utils import get_model_name_from_path
+
+
+load_dotenv()
+
+HF_TOKEN = os.environ["HF_TOKEN"]
+HF_USERNAME = os.environ["HF_USERNAME"]
 
 
 def merge_lora(args):
@@ -9,13 +20,33 @@ def merge_lora(args):
 
     model.save_pretrained(args.save_model_path)
     tokenizer.save_pretrained(args.save_model_path)
-
+    
+    print(f"Merged model saved to {args.save_model_path}")
+    
+    if args.upload:
+        print('Uploading merged model to Hugging Face Hub...')
+        api = HfApi(token=HF_TOKEN)
+        repo_name = args.save_model_path.split('/')[-1]
+        username = HF_USERNAME
+        repo_id = f"{username}/{repo_name}"
+        print(f'saving merged model to {repo_id}')
+        
+        api.create_repo(repo_id=repo_id, repo_type="model", exist_ok=True)
+        api.upload_folder(
+            folder_path=args.save_model_path, 
+            repo_id=repo_id, 
+            repo_type="model"
+        )
+        
+        print('Uploaded merged model to Hugging Face Hub.')
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, required=True)
     parser.add_argument("--model-base", type=str, required=True)
     parser.add_argument("--save-model-path", type=str, required=True)
+    parser.add_argument("--upload", type=bool, default=False)
 
     args = parser.parse_args()
 
