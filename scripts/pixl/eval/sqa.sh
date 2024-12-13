e0fe33a008239041203dbffe51cc48d77fc68ed4
Hunar Batra <i@hunarbatra.com>
2024-12-13 16:22:59 +0000

    add sqa eval script

--- scripts/pixl/eval/sqa.sh
+++ scripts/pixl/eval/sqa.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-#model_name=PIXLGemma-v0-2b-finetune
-model_name=PiXLLaVAPhi2-v1-3b-finetune
-SLM=phi_2
+SLM=phi_35
+model_name=PiXLLaVAPhi35-v2-3b-merged
 VIT=siglip
-# MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
+CONV_MODE=phi3
 
-SLM=phi_2
-model_name=PiXLLaVAPhi2-v2-3b-finetune
-VIT=siglip
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
 python3 -m pixl.eval.model_vqa_science \
     --model-path $MODELDIR \
-    --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
+    --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A_roi.json \
     --image-folder ./playground/data/eval/scienceqa/images/test \
     --answers-file ./playground/data/eval/scienceqa/answers/$model_name.jsonl \
     --single-pred-prompt \
     --temperature 0 \
-    --conv-mode v0
+    --conv-mode $CONV_MODE
+
+# model_name=llava-v1.5-13b
 
 python3 pixl/eval/eval_science_qa.py \
     --base-dir ./playground/data/eval/scienceqa \
