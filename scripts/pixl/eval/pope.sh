c09756538cc493eef4bcf804b07aa29a0ca5ce67
Hunar Batra <i@hunarbatra.com>
2024-12-13 16:22:17 +0000

    add pope script

--- scripts/pixl/eval/pope.sh
+++ scripts/pixl/eval/pope.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#model_name=PIXLGemma-v0-2b-finetune
-model_name=PIXLPhi2-v0-3b-finetune
-SLM=phi_2
+SLM=phi_35
+model_name=PiXLLaVAPhi35-v2-3b-merged
 VIT=siglip
+CONV_MODE=phi3
+
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
 python -m pixl.eval.model_vqa_loader \
     --model-path $MODELDIR \
-    --question-file ./playground/data/eval/pope/llava_pope_test.jsonl \
-    --image-folder /path/to/data/coco/val2014 \
+    --question-file ./playground/data/eval/pope/llava_pope_test_roi.jsonl \
+    --image-folder ./playground/data/eval/pope/images \
     --answers-file ./playground/data/eval/pope/answers/$model_name.jsonl \
     --temperature 0 \
-    --conv-mode phi
+    --conv-mode $CONV_MODE
+
+# model_name=llava-v1.5-13b
 
 python pixl/eval/eval_pope.py \
     --annotation-dir ./playground/data/eval/pope/coco \
