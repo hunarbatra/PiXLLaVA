ba86d415c58fe3c2a0a4565784d1fd5b710d94f7
Hunar Batra <i@hunarbatra.com>
2024-12-13 16:20:11 +0000

    update vqa science eval

--- pixl/eval/model_vqa_science.py
+++ pixl/eval/model_vqa_science.py
@@ -2,16 +2,15 @@ import argparse
 import torch
 import os
 import json
-from tqdm import tqdm
 import shortuuid
+
+from tqdm import tqdm
 
 from pixl.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from pixl.conversation import conv_templates, SeparatorStyle
 from pixl.model.builder import load_pretrained_model
 from pixl.utils import disable_torch_init
 from pixl.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria, process_images
-
-from transformers import DetrImageProcessor, DetrForObjectDetection
 
 from PIL import Image
 import math
@@ -27,25 +26,13 @@ def get_chunk(lst, n, k):
     chunks = split_list(lst, n)
     return chunks[k]
 
-def init_detr_model():
-    # Initialize the DETR model and processor
-    detr_model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-    detr_processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-    
-    # Set DETR model to evaluation mode and disable gradients
-    detr_model.eval()
-    for param in detr_model.parameters():
-        param.requires_grad = False
-    
-    return detr_model, detr_processor
 
 def eval_model(args):
     # Model
     disable_torch_init()
-    detr_model, detr_processor = init_detr_model()
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
+    tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name, flash_attn=True)
 
     questions = json.load(open(os.path.expanduser(args.question_file), "r"))
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
@@ -58,11 +45,15 @@ def eval_model(args):
         question = line['conversations'][0]
         qs = question['value'].replace('<image>', '').strip()
         cur_prompt = qs
+        
+        ram_tags = line['ram_tags'] if 'ram_tags' in line else ''
+        select_tags = line['select_tags'] if 'select_tags' in line else ''
+        bboxes = line['bboxes'] if 'bboxes' in line else []
 
         if 'image' in line:
             image_file = line["image"]
-            image = Image.open(os.path.join(args.image_folder, image_file)) 
-            image_tensor, bboxes_list = process_images([image], image_processor, model.config, detr_model, detr_processor)
+            img_path = os.path.join(args.image_folder, image_file)
+            image_tensor, bboxes_list = process_images(img_path, bboxes, image_processor, model.config)
             
             images = image_tensor.unsqueeze(0).half().cuda()[0]
             bboxes_list = bboxes_list.unsqueeze(0).half().cuda()[0]
@@ -90,7 +81,7 @@ def eval_model(args):
         input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
         attention_mask = input_ids != tokenizer.pad_token_id  # Generate attention mask for text
 
-        stop_str = conv.sep2
+        stop_str = conv.sep2 if conv.sep2 else conv.sep
         keywords = [stop_str]
         stopping_criteria = [KeywordsStoppingCriteria(keywords, tokenizer, input_ids)]
 
@@ -111,10 +102,13 @@ def eval_model(args):
 
         input_token_len = input_ids.shape[1]
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        
         if n_diff_input_output > 0:
             print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
+            
         outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
         outputs = outputs.strip()
+        
         if outputs.endswith(stop_str):
             outputs = outputs[:-len(stop_str)]
         outputs = outputs.strip()
@@ -159,6 +153,7 @@ def eval_model(args):
                                    "metadata": {}}) + "\n")
         ans_file.flush()
     ans_file.close()
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
