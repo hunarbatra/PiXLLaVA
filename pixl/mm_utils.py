f7e915e6f0c59d5e7f8e294b756cbe91659b5969
Hunar Batra <i@hunarbatra.com>
2024-12-13 16:15:26 +0000

    update img processing for pixllava

--- pixl/mm_utils.py
+++ pixl/mm_utils.py
@@ -1,15 +1,28 @@
 from PIL import Image
 from io import BytesIO
 import base64
+import os
+import shutil
+from datetime import datetime
 
 import torch
 from transformers import StoppingCriteria
 from pixl.constants import IMAGE_TOKEN_INDEX
+from PIL import Image
+
+from models.roi_selection import ROISelection
 
 
 def load_image_from_base64(image):
     return Image.open(BytesIO(base64.b64decode(image)))
 
+def remove_yolo_folders(base_path='./'):
+    current_year = str(datetime.now().year)
+    for item in os.listdir(base_path):
+        item_path = os.path.join(base_path, item)
+        if os.path.isdir(item_path) and item.startswith(current_year):
+            print(f"Removing folder: {item_path}")
+            shutil.rmtree(item_path)
 
 def expand2square(pil_img, background_color):
     width, height = pil_img.size
@@ -24,77 +37,61 @@ def expand2square(pil_img, background_color):
         result.paste(pil_img, ((height - width) // 2, 0))
         return result
 
-
-def process_images(images, image_processor, model_cfg, detr_model, detr_processor):
-    device = next(detr_model.parameters()).device
-    
+def process_images(image, bboxes, image_processor, model_cfg):
     image_aspect_ratio = getattr(model_cfg, "image_aspect_ratio", None)
     image_crops = []
     bboxes_list = []
+        
+    # include the original image bbox
+    bboxes_list.append([0.5, 0.5, 1.0, 1.0])
     
-    for image in images:
-        # process image through DETR to get object crops and bounding boxes
-        detr_inputs = detr_processor(images=image, return_tensors="pt").to(device)
-        with torch.no_grad():
-            detr_outputs = detr_model(**detr_inputs)
-        target_sizes = torch.tensor([image.size[::-1]]).to(device)
-        results = detr_processor.post_process_object_detection(detr_outputs, target_sizes=target_sizes)[0]
+    image = Image.open(image).convert('RGB')
+    
+    # pad image to square if needed: default for 'image_aspect_ratio' is 'square'
+    if model_cfg.image_aspect_ratio == 'pad':
+        background_color = [int(x * 255) for x in model_cfg.image_processor.image_mean]
+        image = expand2square(image, background_color)
         
-        bboxes = []
+    # create image crops
+    image_crops = [image]
+    w, h = image.size
+    max_crops = 5
+    
+    for box in bboxes: 
+        x1, y1, x2, y2 = box
+        x_center = (x1 + x2) / 2 / w # get x center and normalize to image width
+        y_center = (y1 + y2) / 2 / h # get y center and normalize to image height
+        width = (x2 - x1) / w # get width and normalize to image width
+        height = (y2 - y1) / h # get height and normalize to image height
+        bbox = [x_center, y_center, width, height]
+        bboxes_list.append(bbox)
         
-        if len(results['scores'] > 0):
-            sorted_indices = torch.argsort(results['scores'], descending=True)
-            sorted_boxes = results['boxes'][sorted_indices].tolist()
-            bboxes = sorted_boxes[:10]
-        else:
-            bboxes = []
-            
-        # include the original image bbox
-        bboxes_list.append([0.5, 0.5, 1.0, 1.0])
+        # crop the image
+        crop_box = [int(x1), int(y1), int(x2), int(y2)]
+        image_crop = image.crop(crop_box)
+        image_crops.append(image_crop)
         
-        # create image crops
-        image_crops = [image]
-        w, h = image.size
+        if len(image_crops) == max_crops + 1:
+            break
         
-        for box in bboxes: 
-            x1, y1, x2, y2 = box
-            x_center = (x1 + x2) / 2 / w
-            y_center = (y1 + y2) / 2 / h
-            width = (x2 - x1) / w
-            height = (y2 - y1) / h
-            bbox = [x_center, y_center, width, height]
-            bboxes_list.append(bbox)
-            
-            # crop the image
-            crop_box = [int(x1), int(y1), int(x2), int(y2)]
-            image_crop = image.crop(crop_box)
-            image_crops.append(image_crop)
-            
-            if len(image_crops) == 10:
-                break
-            
-        if image_aspect_ratio == 'pad':
-            image_crops = [expand2square(img, tuple(int(x*255) for x in image_processor.image_mean)) for img in image_crops]
+    images_tensors = image_processor(images=image_crops, return_tensors='pt')['pixel_values'] # shape: [len(image_crops), 3, 384, 384]
+    bboxes_tensor = torch.tensor(bboxes_list) # shape: [len(image_crops), 4]
         
-        images_tensors = image_processor(images=image_crops, return_tensors='pt')['pixel_values'] # shape: [len(image_crops), 3, 384, 384]
-        bboxes_tensor = torch.tensor(bboxes_list) # shape: [len(image_crops), 4]
-            
     return images_tensors, bboxes_tensor
         
-# def process_images(images, image_processor, model_cfg):
-#     image_aspect_ratio = getattr(model_cfg, "image_aspect_ratio", None)
-#     new_images = []
-#     if image_aspect_ratio == 'pad':
-#         for image in images:
-#             image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
-#             image = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-#             new_images.append(image)
-#     else:
-#         return image_processor(images, return_tensors='pt')['pixel_values']
-#     if all(x.shape == new_images[0].shape for x in new_images):
-#         new_images = torch.stack(new_images, dim=0)
-#     return new_images
-
+def process_images_llava(images, image_processor, model_cfg):
+    image_aspect_ratio = getattr(model_cfg, "image_aspect_ratio", None)
+    new_images = []
+    if image_aspect_ratio == 'pad':
+        for image in images:
+            image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
+            image = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            new_images.append(image)
+    else:
+        return image_processor(images, return_tensors='pt')['pixel_values']
+    if all(x.shape == new_images[0].shape for x in new_images):
+        new_images = torch.stack(new_images, dim=0)
+    return new_images
 
 def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None):
     prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split('<image>')]
@@ -116,7 +113,6 @@ def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX
         raise ValueError(f'Unsupported tensor type: {return_tensors}')
     return input_ids
 
-
 def get_model_name_from_path(model_path):
     model_path = model_path.strip("/")
     model_paths = model_path.split("/")
@@ -124,7 +120,7 @@ def get_model_name_from_path(model_path):
         return model_paths[-2] + "_" + model_paths[-1]
     else:
         return model_paths[-1]
-
+    
 
 class KeywordsStoppingCriteria(StoppingCriteria):
     def __init__(self, keywords, tokenizer, input_ids):
