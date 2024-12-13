d0f77d268b6e3534d47f86be26a1cfd0b5c79bab
Hunar Batra <i@hunarbatra.com>
2024-12-13 16:21:48 +0000

    update textvqa script

--- scripts/pixl/eval/textvqa.sh
+++ scripts/pixl/eval/textvqa.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#model_name=PIXLGemma-v0-2b-finetune
-model_name=PIXLPhi2-v0-3b-finetune
-SLM=phi_2
+SLM=phi_35
+model_name=PiXLLaVAPhi35-v2-3b-merged
 VIT=siglip
+CONV_MODE=phi3
+
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
 python -m pixl.eval.model_vqa_loader \
     --model-path $MODELDIR \
-    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
-    --image-folder /data/team/zhumj/data/finetune/data/textvqa/train_images \
+    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr_roi.jsonl \
+    --image-folder ./playground/data/eval/textvqa/train_images \
     --answers-file ./playground/data/eval/textvqa/answers/$model_name.jsonl \
     --temperature 0 \
-    --conv-mode v0
+    --conv-mode $CONV_MODE
+
+# model_name=llava-v1.5-13b
 
 python -m pixl.eval.eval_textvqa \
     --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
