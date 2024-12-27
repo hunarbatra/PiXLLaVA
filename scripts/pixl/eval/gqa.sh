5abbda0b0db854dfd20e1dc5b0c4f856ebed2775
Hunar Batra <i@hunarbatra.com>
2024-12-27 15:56:03 +0000

    update gqa eval script

--- scripts/pixl/eval/gqa.sh
+++ scripts/pixl/eval/gqa.sh
@@ -6,12 +6,12 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 CHUNKS=${#GPULIST[@]}
 
 
-SPLIT="llava_gqa_testdev_balanced"
+SPLIT="llava_gqa_testdev_balanced_roi"
 GQADIR="./playground/data/eval/gqa/data"
 
 #model_name=PIXLGemma-v0-2b-finetune
-model_name=PIXLPhi2-v0-3b-finetune
-SLM=phi_2
+model_name=PiXLLaVAPhi35-3b-merged
+SLM=phi_35
 VIT=siglip
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
@@ -19,9 +19,9 @@ CKPT=$MODEL_NAME
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m pixl.eval.model_vqa_loader \
-        --model-path ./checkpoints_all/alpaca_sft_phi_2_pre_f_vit/$model_name \
+        --model-path ./ckpts/checkpoints-$VIT/$SLM/$model_name \
         --question-file ./playground/data/eval/gqa/$SPLIT.jsonl \
-        --image-folder /path/to/data/gqa/images \
+        --image-folder ./playground/data/eval/gqa/data/images \
         --answers-file ./playground/data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
         --num-chunks $CHUNKS \
         --chunk-idx $IDX \
@@ -36,12 +36,16 @@ output_file=./playground/data/eval/gqa/answers/$SPLIT/$CKPT/merge.jsonl
 # Clear out the output file if it exists.
 > "$output_file"
 
+# CKPT=llava-v1.5-13b
+# model_name=llava-v1.5-13b
+# CHUNKS=${#GPULIST[@]}
+
 # Loop through the indices and concatenate each file.
 for IDX in $(seq 0 $((CHUNKS-1))); do
     cat ./playground/data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
 done
 
-python scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/testdev_balanced_predictions.json
+python3 ./scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/testdev_balanced_predictions.json
 
 cd $GQADIR
-python eval/eval.py --tier testdev_balanced
+python eval.py --tier testdev_balanced
