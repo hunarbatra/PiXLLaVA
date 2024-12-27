3089b9bac875056955abbc8c39e8a16eda2a9cf7
Hunar Batra <i@hunarbatra.com>
2024-12-27 15:59:46 +0000

    update mmbench.sh

--- scripts/pixl/eval/mmbench.sh
+++ scripts/pixl/eval/mmbench.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-SPLIT="mmbench_dev_20230712"
+SPLIT="mmbench_dev_20230712_roi"
 
-#model_name=PIXLGemma-v0-2b-finetune
-model_name=PIXLPhi2-v0-3b-finetune
-SLM=phi_2
+SLM=phi_35
+model_name=PiXLLaVAPhi35-3b-merged
 VIT=siglip
+CONV_MODE=phi3
+
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
 python -m pixl.eval.model_vqa_mmbench \
     --model-path $MODELDIR \
     --question-file ./playground/data/eval/mmbench/$SPLIT.tsv \
     --answers-file ./playground/data/eval/mmbench/answers/$SPLIT/$model_name.jsonl \
+    --image-folder ./playground/data/eval/mmbench \
     --single-pred-prompt \
     --temperature 0 \
-    --conv-mode phi
+    --conv-mode $CONV_MODE
 
 mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+
+# model_name=llava-v1.5-13b
+# SPLIT="mmbench_dev_20230712"
 
 python scripts/convert_mmbench_for_submission.py \
     --annotation-file ./playground/data/eval/mmbench/$SPLIT.tsv \
