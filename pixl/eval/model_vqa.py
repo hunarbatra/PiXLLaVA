8311b33ac33cdeec847f36a8d75a3bfa834ef900
Hunar Batra <i@hunarbatra.com>
2024-12-27 15:53:14 +0000

    model vqa: add support for no pixl format and attention mask

--- pixl/eval/model_vqa.py
+++ pixl/eval/model_vqa.py
@@ -29,6 +29,7 @@ def get_chunk(lst, n, k):
 def eval_model(args):
     # Model
     disable_torch_init()
+    use_pixl = not args.no_pixl
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name, flash_attn=True)
@@ -63,7 +64,7 @@ def eval_model(args):
         image = Image.open(os.path.join(args.image_folder, image_file)).convert('RGB')
         
         img_path = os.path.join(args.image_folder, image_file)
-        image_tensor, bboxes_list = process_images(img_path, bboxes, image_processor, model.config)
+        image_tensor, bboxes_list = process_images(img_path, bboxes, image_processor, model.config, use_pixl=use_pixl)
         
         images = image_tensor.unsqueeze(0).half().cuda()[0]
         bboxes_list = bboxes_list.unsqueeze(0).half().cuda()[0]
@@ -78,7 +79,7 @@ def eval_model(args):
         with torch.inference_mode():
             output_ids = model.generate(
                 input_ids,
-                # attention_mask=attention_mask,
+                attention_mask=attention_mask,
                 images=images_list,
                 bbox_coords=bbox_list,
                 do_sample=True if args.temperature > 0 else False,
@@ -127,6 +128,7 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--top_p", type=float, default=None)
     parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--no-pixl", action="store_true", default=False)
     args = parser.parse_args()
 
     eval_model(args)
