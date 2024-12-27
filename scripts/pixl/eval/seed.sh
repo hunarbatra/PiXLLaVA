af30e2961866e76b47374c16d3b6378a160407c7
Hunar Batra <i@hunarbatra.com>
2024-12-27 16:00:49 +0000

    update seed.sh script

--- scripts/pixl/eval/seed.sh
+++ scripts/pixl/eval/seed.sh
@@ -5,24 +5,25 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
 
-CKPT=PIXLPhi2-v0-3b-finetune
+CKPT=PIXLLaVAPhi35-3b-merged
 
 #model_name=PIXLGemma-v0-2b-finetune
-model_name=PIXLPhi2-v0-3b-finetune
-SLM=phi_2
+model_name=PiXLLaVAPhi35-3b-merged
+SLM=phi_35
 VIT=siglip
+CONV_MODE=phi3
 MODELDIR=./ckpts/checkpoints-$VIT/$SLM/$model_name
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m pixl.eval.model_vqa_loader \
         --model-path $MODELDIR \
-        --question-file ./playground/data/eval/seed_bench/llava-seed-bench.jsonl \
+        --question-file ./playground/data/eval/seed_bench/llava-seed-bench_roi-spatial.jsonl \
         --image-folder ./playground/data/eval/seed_bench \
         --answers-file ./playground/data/eval/seed_bench/answers/$CKPT/${CHUNKS}_${IDX}.jsonl \
         --num-chunks $CHUNKS \
         --chunk-idx $IDX \
         --temperature 0 \
-        --conv-mode phi &
+        --conv-mode $CONV_MODE &
 done
 
 wait
@@ -31,6 +32,10 @@ output_file=./playground/data/eval/seed_bench/answers/$CKPT/merge.jsonl
 
 # Clear out the output file if it exists.
 > "$output_file"
+
+# CKPT=llava-v1.5-13b
+# model_name=llava-v1.5-13b
+# CHUNKS=${#GPULIST[@]}
 
 # Loop through the indices and concatenate each file.
 for IDX in $(seq 0 $((CHUNKS-1))); do
